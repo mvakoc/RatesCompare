ef6b245cf65518aaed291b44f2e4471a5513cf9d
Michael Vakoc <mivakoc@microsoft.com>
2016-12-23 16:32:16 +0100

    heoatnu

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-var fetch = require('node-fetch')
-var country = require('countryjs')
-var _ = require('lodash')
+'use strict';
+
+var fetch = require('node-fetch');
+var country = require('countryjs');
+var _ = require('lodash');
 var json2csv = require('json2csv');
-var restify = require('restify')
+var restify = require('restify');
 
-var server = restify.createServer()
-server.listen(process.env.PORT || 3978, () => {
-    console.log('server started')
-})
+var server = restify.createServer();
+server.listen(process.env.PORT || 3978, function() {
+    console.log('server started');
+});
 
-server.get('/rates', (req, res) => {
-    var countries = country
-        .all()
-        .map(x => x.ISO.alpha2)
-        .filter(x => !!x)
-        .map(x => fetch(`https://account.viber.com/api/web/rates/${x}`).then(f => f.json()).catch(() => console.log("error")))
+server.get('/rates', function(req, res) {
+    var countries = country.all().map(function(x) {
+        return x.ISO.alpha2;
+    }).filter(function(x) {
+        return !!x;
+    }).map(function(x) {
+        return fetch('https://account.viber.com/api/web/rates/' + x).then(function(f) {
+            return f.json();
+        }).catch(function() {
+            return console.log("error");
+        });
+    });
 
-    Promise.all(countries).then(x =>
-            x.filter(y => y.status === 'success')
-            .map(y => y.data.rates)
-            .map(y => y.map(f => {
+    Promise.all(countries).then(function(x) {
+        return x.filter(function(y) {
+            return y.status === 'success';
+        }).map(function(y) {
+            return y.data.rates;
+        }).map(function(y) {
+            return y.map(function(f) {
                 return {
                     'Country Code': f.country_code,
                     'Name': f.destination,
                     'Value': f.rate.real_value,
                     'Currency': f.rate.value.currency_name
-                }
-            }))
-        ).then(x => _.flatten(x))
-        .then(x => json2csv({ data: x, fields: ['Country Code', 'Name', 'Value', 'Currency'] }))
-        .then(x => res.send(x));
-})
+                };
+            });
+        });
+    }).then(function(x) {
+        return _.flatten(x);
+    }).then(function(x) {
+        return json2csv({ data: x, fields: ['Country Code', 'Name', 'Value', 'Currency'] });
+    }).then(function(x) {
+        return res.send(x);
+    });
+});
