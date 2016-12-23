651fd869254f4181f6f2de49138f5d5b82055ca9
Michael Vakoc <mivakoc@microsoft.com>
2016-12-23 16:11:20 +0100

    STuff

--- index.js
+++ index.js
@@ -3,31 +3,32 @@ var country = require('countryjs')
 var _ = require('lodash')
 var json2csv = require('json2csv');
 var fs = require('fs');
+var restify = require('restify')
 
-var countries = country
-    .all()
-    .map(x => x.ISO.alpha2)
-    .filter(x => !!x)
-    .map(x => fetch(`https://account.viber.com/api/web/rates/${x}`).then(f => f.json()).catch(() => console.log("error")))
+var server = restify.createServer()
+server.listen(process.env.PORT | 3978, () => {
+    console.log('server started')
+})
 
-Promise.all(countries).then(x =>
-        x.filter(y => y.status === 'success')
-        .map(y => y.data.rates)
-        .map(y => y.map(f => {
-            return {
-                'Country Code': f.country_code,
-                'Name': f.destination,
-                'Value': f.rate.real_value,
-                'Currency': f.rate.value.currency_name
-            }
-        }))
-    ).then(x => _.flatten(x))
-    .then(x => json2csv({ data: x, fields: ['Country Code', 'Name', 'Value', 'Currency'] }))
-    .then(x =>
-        fs.writeFile("test.csv", x, function(err) {
-            if (err) {
-                return console.log(err);
-            }
+server.get('/rates', (req, res) => {
+    var countries = country
+        .all()
+        .map(x => x.ISO.alpha2)
+        .filter(x => !!x)
+        .map(x => fetch(`https://account.viber.com/api/web/rates/${x}`).then(f => f.json()).catch(() => console.log("error")))
 
-            console.log("The file was saved!");
-        }));
+    Promise.all(countries).then(x =>
+            x.filter(y => y.status === 'success')
+            .map(y => y.data.rates)
+            .map(y => y.map(f => {
+                return {
+                    'Country Code': f.country_code,
+                    'Name': f.destination,
+                    'Value': f.rate.real_value,
+                    'Currency': f.rate.value.currency_name
+                }
+            }))
+        ).then(x => _.flatten(x))
+        .then(x => json2csv({ data: x, fields: ['Country Code', 'Name', 'Value', 'Currency'] }))
+        .then(x => res.send(x));
+})
